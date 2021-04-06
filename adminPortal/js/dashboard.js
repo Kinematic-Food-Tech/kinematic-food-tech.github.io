a4888bd955726603e24b00fef193591f0ff14457
Gaurav Gupta <gaurav.gupta@goldenpi.com>
2021-04-06 21:03:45 +0530

    dashboard  js created and initial steps added

---
+++ adminPortal/js/dashboard.js
@@ -0,0 +1,40 @@
+var dashboard = {
+    init: function () {
+        $(".nav-menu-side").on("click", function (event) {
+            dashboard.handleSteps(event);
+        });
+    },
+    handleSteps: function (event) {
+        var element = $(event.currentTarget)[0];
+        switch (element.name) {
+            case "addUser":
+                $(".nav-menu-side").removeClass("active");
+                $(element).addClass("active");
+                $(".d-s").removeClass("active-step");
+                $(".dashboard-step-1").addClass("active-step");
+                break;
+            case "analytics":
+                $(".nav-menu-side").removeClass("active");
+                $(element).addClass("active");
+                $(".d-s").removeClass("active-step");
+                $(".dashboard-step-2").addClass("active-step");
+                break;
+            case "ordering":
+                $(".nav-menu-side").removeClass("active");
+                $(element).addClass("active");
+                $(".d-s").removeClass("active-step");
+                $(".dashboard-step-3").addClass("active-step");
+                break;
+            case "approveUsers":
+                $(".nav-menu-side").removeClass("active");
+                $(element).addClass("active");
+                $(".d-s").removeClass("active-step");
+                $(".dashboard-step-4").addClass("active-step");
+                break;
+        }
+    },
+};
+
+$(document).ready(function () {
+    dashboard.init();
+});
