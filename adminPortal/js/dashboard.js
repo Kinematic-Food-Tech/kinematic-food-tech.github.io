bb18f4edbb0e5ec4fcc970f7e486420dd8ad6d57
Gaurav Gupta <gaurav.gupta@goldenpi.com>
2021-04-17 20:25:17 +0530

    dashboard-step-1 jquery task to add rows and make object once user enters data and clicks on go button

--- adminPortal/js/dashboard.js
+++ adminPortal/js/dashboard.js
@@ -6,6 +6,12 @@ var dashboard = {
         $(".login-mobile").on("click", function (event) {
             dashboard.handleMobileLogin();
         });
+        $(".add-rows").on("click", function (event) {
+            dashboard.addMoreUsers(event);
+        });
+        $(".add-user .item .small-button").on("click", function (event) {
+            dashboard.saveUserToDB(event);
+        });
     },
     handleSteps: function (event) {
         var element = $(event.currentTarget)[0];
@@ -38,6 +44,53 @@ var dashboard = {
     },
     handleMobileLogin: function () {
         $("#otp-modal").modal("show");
+    },
+    addMoreUsers: function (event) {
+        const element = $(event.currentTarget)[0];
+        let addRows = '<div class="add-user">';
+        addRows += '<div class="item f-l">';
+        addRows += "<input ";
+        addRows += ' type="text"';
+        addRows += ' name="Name"';
+        addRows += ' placeholder="Name"';
+        addRows += ' class="f-input-medium"';
+        addRows += ' value=""';
+        addRows += " />";
+        addRows += "</div>";
+        addRows += '<div class="item f-l">';
+        addRows += "<input";
+        addRows += ' type="email"';
+        addRows += ' name="Name"';
+        addRows += ' placeholder="Email"';
+        addRows += ' class="f-input-medium"';
+        addRows += ' value=""';
+        addRows += "/>";
+        addRows += "</div>";
+        addRows += '<div class="item f-l">';
+        addRows += "<input";
+        addRows += ' type="text"';
+        addRows += ' name="Name"';
+        addRows += ' placeholder="Mobile"';
+        addRows += ' class="f-input-medium"';
+        addRows += ' value=""';
+        addRows += " /></div>";
+        addRows +=
+            '<div class="item f-l"><button class="small-button">Go</button>';
+        addRows += '</div><div class="clr"></div></div>';
+
+        $(".add-user-container").append(addRows);
+    },
+    saveUserToDB: function (event) {
+        var element = $(event.currentTarget)[0];
+        const input = $(element).parent().parent().find("input");
+        let userData = {
+            name: $(input[0]).val(),
+            email: $(input[1]).val(),
+            mobile: $(input[2]).val(),
+        };
+
+        // use this data to do ajax call
+        console.log("userData", userData);
     },
 };
 
